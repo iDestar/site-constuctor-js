10194f73e66d1e5028715915331580ef12cfc64a
iDestar <destarpwnz@gmai.com>
2020-12-16 07:06:39 +0300

    sum

---
+++ main.js
@@ -0,0 +1,63 @@
+const model = [
+  {
+    type: 'title',
+    value: 'Hi',
+  },
+  { type: 'text', value: 'some text' },
+  { type: 'columns', value: ['first', 'second', 'third'] },
+  { type: 'img', value: './assets/img.png' },
+];
+
+const siteEl = document.querySelector('#site');
+
+model.forEach((block) => {
+  let html = '';
+
+  if (block.type === 'title') {
+    html = title(block);
+  } else if (block.type === 'text') {
+    html = text(block);
+  } else if (block.type === 'columns') {
+    html = column(block);
+  } else if (block.type === 'img') {
+    html = img(block);
+  }
+
+  siteEl.insertAdjacentHTML('beforeend', html);
+});
+
+function title(block) {
+  return `
+    <div class="row">            
+       <div class="col-sm">  
+           <h1>${block.value}</h1>
+       </div>
+    </div>
+    `;
+}
+
+function text(block) {
+  return `
+    <div class="row">
+        <div class="col-sm">
+            <p>${block.value}</p>
+        </div>
+    </div>`;
+}
+
+function column(block) {
+  const html = block.value.map((item) => `<div class="col-sm">${item}</div>`);
+  return `
+    <div class="row">
+        ${html.join('')}
+    </div>
+   `;
+}
+
+function img(block) {
+  return `
+     <div class="row">
+        <img src="${block.value}">
+     </div>
+    `;
+}
